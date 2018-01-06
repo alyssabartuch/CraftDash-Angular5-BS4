3d35aa8dddb254b9cd5a1bfd19688db1707c79d5
alyssa_bartuch <alyssabartuch@gmail.com>
2018-01-06 16:43:40 -0600

    library cards

--- src/app/layout/bs-element/bs-element.component.ts
+++ src/app/layout/bs-element/bs-element.component.ts
@@ -10,5 +10,81 @@ import { routerTransition } from '../../router.animations';
 export class BsElementComponent implements OnInit {
     constructor() {}
 
+    cards = [
+      {
+        type: "knitting",
+        title: "Header",
+        thumbnail: "https://d1a6t1943usoj7.cloudfront.net/v1/1a01ab15-bd66-4578-824a-cd3741daaa1e/medium/1470696788032-knitting_yarn_20160722_002_null.jpg",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "spinning",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "crochet",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "spinning",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "crochet",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      },
+      {
+        type: "knitting",
+        title: "Header",
+        subtitle: "Primary card title",
+        content: "Some quick example text to build on the card title and make up the bulk of the card's content"
+      }
+    ];
+
     ngOnInit() {}
 }
