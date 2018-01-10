f518b6db9d04e1b41d5dc437726a374759bcfe97
alyssa_bartuch <alyssabartuch@gmail.com>
2018-01-10 13:53:15 -0600

    developed web resource tiles

--- src/app/layout/web-resources/web-resources.component.ts
+++ src/app/layout/web-resources/web-resources.component.ts
@@ -13,10 +13,41 @@ export class WebResourcesComponent implements OnInit {
     webResources = [
       {
         title: 'ravelry',
-        craft: 'All',
+        subtitle: 'Ravelry is a community site, an organizational tool, and a yarn & pattern database for knitters and crocheters.'
         webAddress: 'https://www.ravelry.com/',
-        img: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj8mOrhr8vYAhXrRN8KHY4fCe0QjRwIBw&url=https%3A%2F%2Fwww.ravelry.com%2F&psig=AOvVaw3mZGq0jQ9_mWbSLnXrY-Pm&ust=1515604200084707'
+        img: 'https://style-cdn.ravelrycache.com/images/splash/ravelry-logo-81r-300x.png'
+      },
+
+      {title: 'craftsy',
+      subtitle: 'Lets make something together: kits, supplies, tutorials and endless inspiration for quilting, knitting, cakes, sewing, cooking, fine art and more.'
+      webAddress: 'https://www.ravelry.com/',
+      img: 'http://d1a6t1943usoj7.cloudfront.net/v1/096818e6-a6d0-4def-ae42-928e125fc699/large/1474658761910-craftsy_final_wordmark_logo.png'
+      },
+
+      {title: 'knitpicks',
+      subtitle: 'Carefully selected luxury yarn, knitting supplies, needles, patterns & books. Free shipping. Complete hand knitting resource.'
+      webAddress: 'https://www.knitpicks.com/',
+      img: 'http://img2.everafterguide.net/W7LpKz-R65vRZI0U_um86WNOIVQ=/0x200/logos/knitpicks.com.png'
+      },
+
+      {title: 'knitters pride',
+      subtitle: 'Knitters Pride offers a variety of high quality knitting and crochet needles. We carry needles from: Symfonie, Platina, MarblZ, Karbonz, Bamboo, Basix, Trendz and Waves.',
+      webAddress: 'https://www.knitterspride.com/',
+      img: 'http://www.knitterspride.com/images/logo.png'
+      },
+
+      {title: 'etsy',
+      subtitle: 'Find handmade, vintage, and unique goods that express who you are.',
+      webAddress: 'https://www.etsy.com/',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Etsy_logo_lg_rgb.png'
+      },
+
+      {title: 'pinterest',
+      subtitle: 'Discover recipes, home ideas, style inspiration and other ideas to try.',
+      webAddress: 'https://www.pinterest.com/',
+      img: 'https://digitalreadymarketing.com/wp-content/uploads/2016/03/pinterest.jpg'
       }
+
     ];
 
 
