117b11775fe9350bea9fb64d9fbd55207203af3f
alyssa_bartuch <alyssabartuch@gmail.com>
2018-01-11 17:58:36 -0600

    general commit

--- src/app/layout/web-resources/web-resources.module.ts
+++ src/app/layout/web-resources/web-resources.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { WebResourcesRoutingModule } from './web-resources-routing.module';
 import { WebResourcesComponent } from './web-resources.component';
+
+import {ModalComponent} from './components';
 import { PageHeaderModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, WebResourcesRoutingModule, PageHeaderModule],
-    declarations: [WebResourcesComponent]
+    imports: [
+        CommonModule,
+        WebResourcesRoutingModule,
+        PageHeaderModule
+    ],
+    declarations: [
+        WebResourcesComponent,
+        ModalComponent
+    ]
 })
 export class WebResourcesModule {}
