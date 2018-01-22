911df33975e5c2d728d07867ca8a66ecd26cc918
alyssa_bartuch <alyssabartuch@gmail.com>
2018-01-21 22:00:42 -0600

    Library upload work

--- src/app/layout/library/library.module.ts
+++ src/app/layout/library/library.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule }   from '@angular/forms';
 
 import { LibraryRoutingModule } from './library-routing.module';
 import { LibraryComponent } from './library.component';
@@ -9,7 +11,7 @@ import {ModalComponent} from './components';
 import { LibraryFormComponent } from './components/library-form/library-form.component';
 
 @NgModule({
-    imports: [CommonModule, LibraryRoutingModule, PageHeaderModule],
+    imports: [CommonModule, FormsModule, NgbModule.forRoot(), LibraryRoutingModule, PageHeaderModule],
     declarations: [LibraryComponent, ModalComponent, LibraryFormComponent]
 })
 export class LibraryModule {}
